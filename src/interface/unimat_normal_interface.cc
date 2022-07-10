ac2a1551e91afb40d300d98f417a28ed5c052e41
Gabriele Albertini <galbertini@holy7c24101.rc.fas.harvard.edu>
2022-07-10 16:58:29 -0400

    add unimat normal

---
+++ src/interface/unimat_normal_interface.cc
@@ -0,0 +1,149 @@
+/**
+ * @file   unimat_normal_interface.cc
+ *
+ * @author David S. Kammer <dkammer@ethz.ch>
+ * @author Gabriele Albertini <ga288@cornell.edu>
+ * @author Chun-Yu Ke <ck659@cornell.edu>
+ *
+ * @date creation: Fri Feb 5 2021
+ * @date last modification: Fri Feb 5 2021
+ *
+ * @brief  TODO
+ *
+ *
+ * Copyright (C) 2021 ETH Zurich (David S. Kammer)
+ *
+ * This file is part of uguca.
+ *
+ * uguca is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uguca is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with uguca.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "unimat_normal_interface.hh"
+#include "half_space.hh"
+
+__BEGIN_UGUCA__
+
+/*-------------------------------------------------------------------------- */
+UnimatNormalInterface::UnimatNormalInterface(FFTableMesh & mesh,
+					   Material & top_material,
+					   InterfaceLaw & law,
+					   const SolverMethod & method) :
+  Interface(mesh, law)
+{
+  this->top = HalfSpace::newHalfSpace(mesh, 1, this->name+"_top", method);
+  
+  this->half_spaces.resize(1);
+  this->half_spaces[0] = this->top;
+
+  this->top->setMaterial(&top_material);
+}
+
+/* -------------------------------------------------------------------------- */
+UnimatNormalInterface::~UnimatNormalInterface() {
+  delete this->top;
+}
+
+/* -------------------------------------------------------------------------- */
+void UnimatNormalInterface::closingNormalGapForce(NodalFieldComponent & close_force,
+						 bool predicting) {
+  // top material information
+  const Material & mat_t = this->top->getMaterial();
+  double cp_t = mat_t.getCp();
+  double cs_t = mat_t.getCs();
+  double mu_t = mat_t.getShearModulus();
+  double eta_t = cp_t / cs_t;
+  double fact_t = this->time_step * cs_t / mu_t / eta_t;
+
+  // accessors
+  double * u_1_t = this->top->getDisp(predicting).storage(1);
+  double * f_1_t = this->top->getInternal().storage(1);
+  double * t0_1 = this->load.storage(1);
+  double * cf = close_force.storage();
+
+  for (int n=0; n<this->mesh.getNbLocalNodes(); ++n) {
+    double u_1_gap = 2.0 * u_1_t[n];           // for readability
+    double du_1_t =  t0_1[n] + f_1_t[n];       // for readability
+    cf[n] = 0.5 * u_1_gap / fact_t + du_1_t;
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+void UnimatNormalInterface::maintainShearGapForce(NodalField & maintain_force) {
+
+  for (int d=0; d<this->mesh.getDim(); d+=2) {
+
+    // accessors
+    double *f_t = this->top->getInternal().storage(d);
+    double *t0 = this->load.storage(d);
+    double *mf = maintain_force.storage(d);
+
+    for (int n=0; n<this->mesh.getNbLocalNodes(); ++n) {
+      mf[n] = t0[n] + 0.0 * f_t[n];
+    }
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+void UnimatNormalInterface::computeGap(NodalField & gap,
+				      bool predicting) {
+
+  for (int d = 0; d < this->mesh.getDim(); ++d) {
+
+    double * top_disp = this->top->getDisp(predicting).storage(d);
+    double * gap_p = gap.storage(d);
+
+    for (int n=0; n<this->mesh.getNbLocalNodes(); ++n) {
+      gap_p[n] = 2 * top_disp[n];
+    }
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+void UnimatNormalInterface::computeGapVelocity(NodalField & gap_velo,
+					      bool predicting) {
+
+  for (int d = 0; d < this->mesh.getDim(); ++d) {
+
+    double * top_velo_p = this->top->getVelo(predicting).storage(d);
+    double * gap_velo_p = gap_velo.storage(d);
+
+    for (int n = 0; n < this->mesh.getNbLocalNodes(); ++n) {
+      gap_velo_p[n] = 2 * top_velo_p[n];
+    }
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+void UnimatNormalInterface::registerDumpField(const std::string & field_name) {
+
+  int d = std::atoi(&field_name[field_name.length()-1]);
+
+  if (d >= this->mesh.getDim())
+    throw std::runtime_error("Field "+field_name
+			     +" cannot be dumped, too high dimension");
+
+  bool registered = false;
+  // field_name starts with "top"
+  if (field_name.rfind("top", 0) == 0) {
+    // cut away "top_" from field_name and give interface as dumper
+    registered = this->top->registerDumpFieldToDumper(field_name.substr(4),
+						      field_name,
+						      this);
+  }
+
+  if (!registered)
+    Interface::registerDumpField(field_name);
+}
+
+__END_UGUCA__
+
