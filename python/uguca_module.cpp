ad14027187988c27d0e7e40985eb18b59b58a1f4
Mohit Pundir <tomohitpundir@gmail.com>
2023-10-19 18:04:47 +0200

    porting SolverMethod enums to python

--- python/uguca_module.cpp
+++ python/uguca_module.cpp
@@ -6,4 +6,15 @@ namespace uguca {
 
   namespace py = pybind11;
 
+  PYBIND11_MODULE(puguca, mod) {
+    mod.doc() = "python module of uguca";
+
+    py::enum_<SolverMethod>(mod, "SolverMethod")
+      .value("static", _static)
+      .value("quasi_dynamic", _quasi_dynamic)
+      .value("dynamic", _dynamic)
+      .value("hybrid", _hybrid)
+      .export_values();
+  }
+
 }
