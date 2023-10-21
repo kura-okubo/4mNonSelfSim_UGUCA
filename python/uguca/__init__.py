055313ec732f6134682254c233ff02f80eb045d6
Mohit Pundir <tomohitpundir@gmail.com>
2023-10-21 20:55:39 +0200

    removed mpi4py import

--- python/uguca/__init__.py
+++ python/uguca/__init__.py
@@ -9,8 +9,3 @@ private_keys = set(dir(_puguca)) - set(dir())
 
 for k in private_keys:
     globals()[k] = getattr(_puguca, k)
-
-try:
-    from mpi4py import MPI  # noqa: F401
-except Exception:
-    pass
