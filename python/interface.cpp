1b1a0bf6d0fabf7cacbfeae315308e1fc09156b1
florez <florez@ethz.ch>
2024-05-31 23:39:53 +0200

    ENH: add LinearShearCohesiveLaw to python interface

--- python/interface.cpp
+++ python/interface.cpp
@@ -1,210 +1,198 @@
-#include "wrap.hh"
-#include "interface_law.hh"
-#include "barras_law.hh"
-#include "linear_coulomb_friction_law.hh"
-#include "uca_base_mesh.hh"
-#include "uca_fftable_mesh.hh"
 #include "interface.hh"
+
+#include "barras_law.hh"
 #include "bimat_interface.hh"
-#include "unimat_shear_interface.hh"
 #include "defrig_interface.hh"
+#include "interface_law.hh"
+#include "linear_coulomb_friction_law.hh"
+#include "linear_shear_cohesive_law.hh"
 #include "material.hh"
 #include "nodal_field.hh"
-
+#include "uca_base_mesh.hh"
 #include "uca_dumper.hh"
+#include "uca_fftable_mesh.hh"
+#include "unimat_shear_interface.hh"
+#include "wrap.hh"
 
-namespace uguca
-{
+namespace uguca {
 
-	namespace wrap
-	{
+namespace wrap {
 
-		using namespace py::literals;
+using namespace py::literals;
 
-		class PyInterfaceLaw : public InterfaceLaw
-		{
-		public:
-			using InterfaceLaw::InterfaceLaw;
+class PyInterfaceLaw : public InterfaceLaw {
+public:
+  using InterfaceLaw::InterfaceLaw;
 
-			void computeCohesiveForces(bool predicting = false, unsigned int ts_factor = 1) override
-			{
-				// NOLINTNEXTLINE
-				PYBIND11_OVERRIDE_PURE(void, InterfaceLaw, computeCohesiveForces, predicting, ts_factor);
-			}
-		};
+  void computeCohesiveForces(bool predicting = false,
+                             unsigned int ts_factor = 1) override {
+    // NOLINTNEXTLINE
+    PYBIND11_OVERRIDE_PURE(void, InterfaceLaw, computeCohesiveForces,
+                           predicting, ts_factor);
+  }
+};
 
-		void wrapInterface(py::module &mod)
-		{
+void wrapInterface(py::module &mod) {
+  py::class_<Interface, std::shared_ptr<Interface>>(mod, "Interface")
+      .def("getLoad", &Interface::getLoad, py::return_value_policy::reference)
+      .def("getCohesion", &Interface::getCohesion,
+           py::return_value_policy::reference);
 
-			py::class_<Interface, std::shared_ptr<Interface>>(mod, "Interface")
-				.def("getLoad",
-					 &Interface::getLoad,
-					 py::return_value_policy::reference)
-				.def("getCohesion",
-					 &Interface::getCohesion,
-					 py::return_value_policy::reference);
+  py::class_<BimatInterface, std::shared_ptr<BimatInterface>, Interface>(
+      mod, "BimatInterface")
+      .def(py::init<FFTableMesh &, SpatialDirectionSet, Material &, Material &,
+                    InterfaceLaw &, const SolverMethod &>(),
+           py::arg("mesh"), py::arg("components"), py::arg("top_material"),
+           py::arg("bot_material"), py::arg("law"),
+           py::arg("method") = _dynamic)
+      .def("init", &BimatInterface::init)
+      .def("setTimeStep", &BimatInterface::setTimeStep)
+      .def("getStableTimeStep", &BimatInterface::getStableTimeStep,
+           py::return_value_policy::reference)
+      .def("initDump",
+           [](BimatInterface &self, const std::string &bname,
+              const std::string &path) { self.initDump(bname, path); })
+      .def("computeGap",
+           [](BimatInterface &self, NodalField &gap, bool predicting) {
+             self.computeGap(gap, predicting);
+           })
+      .def("closingNormalGapForce", &BimatInterface::closingNormalGapForce)
+      .def("maintainShearGapForce", &BimatInterface::maintainShearGapForce)
+      .def("registerDumpFields",
+           [](BimatInterface &self, const std::string &field_names) {
+             self.registerDumpFields(field_names);
+           })
+      .def("dump", [](BimatInterface &self, unsigned int step,
+                      double time) { self.dump(step, time); })
+      .def("getTop", &BimatInterface::getTop,
+           py::return_value_policy::reference)
+      .def("getBot", &BimatInterface::getBot,
+           py::return_value_policy::reference)
+      .def("advanceTimeStep", &BimatInterface::advanceTimeStep,
+           py::arg("solver_method"), py::arg("ts_factor") = 1);
 
-			py::class_<BimatInterface,
-					   std::shared_ptr<BimatInterface>, Interface>(mod, "BimatInterface")
-				.def(py::init<FFTableMesh &, SpatialDirectionSet, Material &, Material &, InterfaceLaw &,
-							  const SolverMethod &>(),
-					 py::arg("mesh"), py::arg("components"), py::arg("top_material"), py::arg("bot_material"), py::arg("law"),
-					 py::arg("method") = _dynamic)
-				.def("init",
-					 &BimatInterface::init)
-				.def("setTimeStep",
-					 &BimatInterface::setTimeStep)
-				.def("getStableTimeStep",
-					 &BimatInterface::getStableTimeStep,
-					 py::return_value_policy::reference)
-				.def("initDump", [](BimatInterface &self, const std::string &bname,
-									const std::string &path)
-					 { self.initDump(bname, path); })
-				.def("computeGap", [](BimatInterface &self, NodalField &gap, bool predicting)
-					 { self.computeGap(gap, predicting); })
-				.def("closingNormalGapForce",
-					 &BimatInterface::closingNormalGapForce)
-				.def("maintainShearGapForce",
-					 &BimatInterface::maintainShearGapForce)
-				.def("registerDumpFields", [](BimatInterface &self, const std::string &field_names)
-					 { self.registerDumpFields(field_names); })
-				.def("dump", [](BimatInterface &self, unsigned int step, double time)
-					 { self.dump(step, time); })
-				.def("getTop", &BimatInterface::getTop,
-					 py::return_value_policy::reference)
-				.def("getBot", &BimatInterface::getBot,
-					 py::return_value_policy::reference)
-				.def("advanceTimeStep",
-					 &BimatInterface::advanceTimeStep,
-					 py::arg("solver_method"), py::arg("ts_factor") = 1);
+  py::class_<UnimatShearInterface, std::shared_ptr<UnimatShearInterface>,
+             Interface>(mod, "UnimatShearInterface")
+      .def(py::init<FFTableMesh &, SpatialDirectionSet, Material &,
+                    InterfaceLaw &, const SolverMethod &>(),
+           py::arg("mesh"), py::arg("components"), py::arg("top_material"),
+           py::arg("law"), py::arg("method") = _dynamic)
+      .def("init", &UnimatShearInterface::init,
+           py::return_value_policy::reference)
+      .def("setTimeStep", &UnimatShearInterface::setTimeStep)
+      .def("getStableTimeStep", &UnimatShearInterface::getStableTimeStep,
+           py::return_value_policy::reference)
+      .def("initDump",
+           [](UnimatShearInterface &self, const std::string &bname,
+              const std::string &path) { self.initDump(bname, path); })
+      .def("registerDumpFields",
+           [](UnimatShearInterface &self, const std::string &field_names) {
+             self.registerDumpFields(field_names);
+           })
+      .def("dump", [](UnimatShearInterface &self, unsigned int step,
+                      double time) { self.dump(step, time); })
+      .def("getTop", &UnimatShearInterface::getTop,
+           py::return_value_policy::reference)
+      .def("getBot", &UnimatShearInterface::getBot,
+           py::return_value_policy::reference)
+      .def("advanceTimeStep", &UnimatShearInterface::advanceTimeStep)
+      .def("getLoad", &UnimatShearInterface::getLoad,
+           py::return_value_policy::reference)
+      .def("computeGap",
+           [](UnimatShearInterface &self, NodalField &gap, bool predicting) {
+             self.computeGap(gap, predicting);
+           })
+      .def("closingNormalGapForce",
+           &UnimatShearInterface::closingNormalGapForce)
+      .def("maintainShearGapForce",
+           &UnimatShearInterface::maintainShearGapForce);
 
-			py::class_<UnimatShearInterface,
-					   std::shared_ptr<UnimatShearInterface>, Interface>(mod,
-																		 "UnimatShearInterface")
-				.def(py::init<FFTableMesh &, SpatialDirectionSet, Material &, InterfaceLaw &,
-							  const SolverMethod &>(),
-					 py::arg("mesh"), py::arg("components"), py::arg("top_material"), py::arg("law"),
-					 py::arg("method") = _dynamic)
-				.def("init",
-					 &UnimatShearInterface::init, py::return_value_policy::reference)
-				.def("setTimeStep",
-					 &UnimatShearInterface::setTimeStep)
-				.def("getStableTimeStep",
-					 &UnimatShearInterface::getStableTimeStep,
-					 py::return_value_policy::reference)
-				.def("initDump", [](UnimatShearInterface &self,
-									const std::string &bname,
-									const std::string &path)
-					 { self.initDump(bname, path); })
-				.def("registerDumpFields", [](UnimatShearInterface &self,
-											  const std::string &field_names)
-					 { self.registerDumpFields(field_names); })
-				.def("dump", [](UnimatShearInterface &self, unsigned int step,
-								double time)
-					 { self.dump(step, time); })
-				.def("getTop", &UnimatShearInterface::getTop,
-					 py::return_value_policy::reference)
-				.def("getBot", &UnimatShearInterface::getBot,
-					 py::return_value_policy::reference)
-				.def("advanceTimeStep",
-					 &UnimatShearInterface::advanceTimeStep)
-				.def("getLoad",
-					 &UnimatShearInterface::getLoad,
-					 py::return_value_policy::reference)
-				.def("computeGap", [](UnimatShearInterface &self, NodalField &gap, bool predicting)
-					 { self.computeGap(gap, predicting); })
-				.def("closingNormalGapForce",
-					 &UnimatShearInterface::closingNormalGapForce)
-				.def("maintainShearGapForce",
-					 &UnimatShearInterface::maintainShearGapForce);
+  py::class_<DefRigInterface, std::shared_ptr<DefRigInterface>>(
+      mod, "DefRigInterface")
+      .def(py::init<FFTableMesh &, SpatialDirectionSet, Material &,
+                    InterfaceLaw &, const SolverMethod &>(),
+           py::arg("mesh"), py::arg("components"), py::arg("top_material"),
+           py::arg("law"), py::arg("method") = _dynamic)
+      .def("init", &DefRigInterface::init)
+      .def("setTimeStep", &DefRigInterface::setTimeStep)
+      .def("getStableTimeStep", &DefRigInterface::getStableTimeStep,
+           py::return_value_policy::reference)
+      .def("initDump",
+           [](DefRigInterface &self, const std::string &bname,
+              const std::string &path) { self.initDump(bname, path); })
+      .def("registerDumpFields",
+           [](DefRigInterface &self, const std::string &field_names) {
+             self.registerDumpFields(field_names);
+           })
+      .def("dump", [](DefRigInterface &self, unsigned int step,
+                      double time) { self.dump(step, time); })
+      .def("getTop", &DefRigInterface::getTop,
+           py::return_value_policy::reference)
+      .def("getBot", &DefRigInterface::getBot,
+           py::return_value_policy::reference)
+      .def("advanceTimeStep", &DefRigInterface::advanceTimeStep)
+      .def("getLoad", &DefRigInterface::getLoad,
+           py::return_value_policy::reference);
 
-			py::class_<DefRigInterface,
-					   std::shared_ptr<DefRigInterface>>(mod,
-														 "DefRigInterface")
-				.def(py::init<FFTableMesh &, SpatialDirectionSet, Material &, InterfaceLaw &,
-							  const SolverMethod &>(),
-					 py::arg("mesh"), py::arg("components"), py::arg("top_material"), py::arg("law"),
-					 py::arg("method") = _dynamic)
-				.def("init",
-					 &DefRigInterface::init)
-				.def("setTimeStep",
-					 &DefRigInterface::setTimeStep)
-				.def("getStableTimeStep",
-					 &DefRigInterface::getStableTimeStep,
-					 py::return_value_policy::reference)
-				.def("initDump", [](DefRigInterface &self,
-									const std::string &bname,
-									const std::string &path)
-					 { self.initDump(bname, path); })
-				.def("registerDumpFields", [](DefRigInterface &self,
-											  const std::string &field_names)
-					 { self.registerDumpFields(field_names); })
-				.def("dump", [](DefRigInterface &self, unsigned int step,
-								double time)
-					 { self.dump(step, time); })
-				.def("getTop", &DefRigInterface::getTop,
-					 py::return_value_policy::reference)
-				.def("getBot", &DefRigInterface::getBot,
-					 py::return_value_policy::reference)
-				.def("advanceTimeStep",
-					 &DefRigInterface::advanceTimeStep)
-				.def("getLoad",
-					 &DefRigInterface::getLoad,
-					 py::return_value_policy::reference);
+  py::class_<InterfaceLaw, std::shared_ptr<InterfaceLaw>, PyInterfaceLaw>(
+      mod, "InterfaceLaw")
+      .def(py::init<BaseMesh &>(), py::arg("mesh"))
+      .def("getCohesion", &InterfaceLaw::getCohesion,
+           py::return_value_policy::reference)
+      .def("getInterface", &InterfaceLaw::getInterface,
+           py::return_value_policy::reference)
+      .def("getMesh", &InterfaceLaw::getMesh,
+           py::return_value_policy::reference)
+      .def("computeCohesiveForces",
+           [](InterfaceLaw &self, bool predicting = false,
+              unsigned int ts_factor = 1) {
+             self.computeCohesiveForces(predicting, ts_factor);
+           });
 
-			py::class_<InterfaceLaw,
-					   std::shared_ptr<InterfaceLaw>, PyInterfaceLaw>(mod, "InterfaceLaw")
-				.def(py::init<BaseMesh &>(),
-					 py::arg("mesh"))
-				.def("getCohesion",
-					 &InterfaceLaw::getCohesion,
-					 py::return_value_policy::reference)
-				.def("getInterface",
-					 &InterfaceLaw::getInterface,
-					 py::return_value_policy::reference)
-				.def("getMesh",
-					 &InterfaceLaw::getMesh,
-					 py::return_value_policy::reference)
-				.def("computeCohesiveForces",
-					 [](InterfaceLaw &self, bool predicting = false, unsigned int ts_factor = 1)
-					 {
-						 self.computeCohesiveForces(predicting, ts_factor);
-					 });
+  py::class_<BarrasLaw, InterfaceLaw, std::shared_ptr<BarrasLaw>>(mod,
+                                                                  "BarrasLaw")
+      .def(py::init<BaseMesh &, double, double, std::string &>(),
+           py::arg("mesh"), py::arg("tau_max_default"),
+           py::arg("delta_c_default"), py::arg("name") = "blaw")
+      .def("getTauMax", &BarrasLaw::getTauMax,
+           py::return_value_policy::reference)
+      .def("getDc", &BarrasLaw::getDc, py::return_value_policy::reference);
 
-			py::class_<BarrasLaw, InterfaceLaw,
-					   std::shared_ptr<BarrasLaw>>(mod, "BarrasLaw")
-				.def(py::init<BaseMesh &, double, double, std::string &>(),
-					 py::arg("mesh"), py::arg("tau_max_default"), py::arg("delta_c_default"),
-					 py::arg("name") = "blaw")
-				.def("getTauMax",
-					 &BarrasLaw::getTauMax, py::return_value_policy::reference)
-				.def("getDc",
-					 &BarrasLaw::getDc, py::return_value_policy::reference);
+  py::class_<LinearCoulombFrictionLaw, InterfaceLaw,
+             std::shared_ptr<LinearCoulombFrictionLaw>>(
+      mod, "LinearCoulombFrictionLaw")
+      .def(
+          py::init<BaseMesh &, double, double, double, double, std::string &>(),
+          py::arg("mesh"), py::arg("mu_s_default"), py::arg("mu_k_default"),
+          py::arg("d_c_default"), py::arg("char_reg_time") = 0.,
+          py::arg("name") = "lcflaw")
+      .def("getMuS", &LinearCoulombFrictionLaw::getMuS,
+           py::return_value_policy::reference)
+      .def("getMuK", &LinearCoulombFrictionLaw::getMuK,
+           py::return_value_policy::reference)
+      .def("getDc", &LinearCoulombFrictionLaw::getDc,
+           py::return_value_policy::reference)
+      .def("getCharacteristicTime",
+           &LinearCoulombFrictionLaw::getCharacteristicTime,
+           py::return_value_policy::reference);
 
-			py::class_<LinearCoulombFrictionLaw, InterfaceLaw,
-					   std::shared_ptr<LinearCoulombFrictionLaw>>(mod,
-																  "LinearCoulombFrictionLaw")
-				.def(py::init<BaseMesh &, double, double, double,
-							  double, std::string &>(),
-					 py::arg("mesh"), py::arg("mu_s_default"),
-					 py::arg("mu_k_default"),
-					 py::arg("d_c_default"),
-					 py::arg("char_reg_time") = 0.,
-					 py::arg("name") = "lcflaw")
-				.def("getMuS",
-					 &LinearCoulombFrictionLaw::getMuS,
-					 py::return_value_policy::reference)
-				.def("getMuK",
-					 &LinearCoulombFrictionLaw::getMuK,
-					 py::return_value_policy::reference)
-				.def("getDc",
-					 &LinearCoulombFrictionLaw::getDc,
-					 py::return_value_policy::reference)
-				.def("getCharacteristicTime",
-					 &LinearCoulombFrictionLaw::getCharacteristicTime,
-					 py::return_value_policy::reference);
-		}
-
-	}
-
+  // ----------------------
+  // LinearShearCohesiveLaw
+  // ----------------------
+  py::class_<LinearShearCohesiveLaw, InterfaceLaw,
+             std::shared_ptr<LinearShearCohesiveLaw>>(mod,
+                                                      "LinearShearCohesiveLaw")
+      .def(py::init<BaseMesh &, double, double, double, std::string &>(),
+           py::arg("mesh"), py::arg("Gc_default"), py::arg("tau_c_default"),
+           py::arg("tau_r_default") = 0., py::arg("name") = "lsclaw")
+      .def("getGc", &LinearShearCohesiveLaw::getGc,
+           py::return_value_policy::reference)
+      .def("getTauc", &LinearShearCohesiveLaw::getTauc,
+           py::return_value_policy::reference)
+      .def("getTaur", &LinearShearCohesiveLaw::getTaur,
+           py::return_value_policy::reference);
 }
+
+} // namespace wrap
+
+} // namespace uguca
