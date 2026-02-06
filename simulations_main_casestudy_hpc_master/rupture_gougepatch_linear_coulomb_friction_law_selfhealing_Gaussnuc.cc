c6442ee200649c84a907f0c8964e43fe11aa6e29
kura-okubo <kura.okubo@gmail.com>
2026-02-06 11:04:11 +0900

    fixed typo

--- simulations_main_casestudy_hpc_master/rupture_gougepatch_linear_coulomb_friction_law_selfhealing_Gaussnuc.cc
+++ simulations_main_casestudy_hpc_master/rupture_gougepatch_linear_coulomb_friction_law_selfhealing_Gaussnuc.cc
@@ -7,7 +7,7 @@ Ke, C.-Y., McLaskey, G. C., and Kammer, D. S. Earthquake breakdown energy scalin
 
 https://gitlab.com/uguca/projects/breakdown_energy_scaling
 
-- new functions: self-healing friction law and the Gaussian stress distribution in the nucleation area.
+- new functinos: self-healing friction law and the Gaussian stress distribution in the nucleation area.
 */
 
 /**
